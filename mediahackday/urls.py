ae5fe110f45de9244e4402faa5e0b5e049dfbe11
Michał Hernas <michal@hernas.pl>
2014-03-22 12:12:55 +0100

    Started working on tokens

--- mediahackday/urls.py
+++ mediahackday/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
-
+from apn.resources import TokenResource
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'mediahackday.views.home', name='home'),
+    url(r'^token/$', TokenResource.as_view(), name='token'),
     # url(r'^mediahackday/', include('mediahackday.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
